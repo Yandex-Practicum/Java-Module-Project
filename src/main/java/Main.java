0580674985fa10eccd25699575e1937efc95f06d
s-buvaka <s-buvaka@yandex-team.ru>
2022-07-27 23:13:52 +0300

    Change project name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        //System.out.println("Привет Мир");
+
+        //static short numberOfPeople;
+
+            People sTart=new People();
+            sTart.countPeople();
+            System.out.println(sTart.numberOfPeople);//удалить в финальной версии
     }
 }
---
+++ src/main/java/People.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class People {
+
+    static Short numberOfPeople;//объявляем статическую переменную для передачи в класс Main
+
+    public static void countPeople() {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Добрый день");
+
+
+        while (true) {
+            System.out.println("На сколько человек разделить счёт?");
+
+            if (scanner.hasNext()){
+                numberOfPeople = scanner.nextShort();
+                if (numberOfPeople > 1) {
+                    System.out.println("Делим счёт на "+ numberOfPeople+ " гостей");//получили число участников
+                    break;
+                }
+
+                if (numberOfPeople <= 1) {
+                    System.out.println("Неправильный ввод. Попробуйте ввести число участников ещё раз.");
+                }
+            }else{
+                System.out.println("Неправильный ввод. Попробуйте ввести число участников в числовом формате.");
+                scanner.nextLine();
+            }
+
+        }
+
+    }
+}
