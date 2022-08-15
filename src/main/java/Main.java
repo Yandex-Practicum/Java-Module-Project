303cd2355f0b904ccaaf51d749045e2e8159258e
purpleratchet <ibeater01@gmail.com>
2022-08-15 13:54:14 +0300

    мой второй комит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,68 +1,9 @@
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
-class Position {
-    String positionName;
-    double positionPrice;
-    Position(String name, double price) {
-        positionPrice = price;
-        positionName = name;
-    }
-    public static double calculate(ArrayList<Position> positions) {
-        int i = 0;
-        double result = 0;
-        while (i < positions.size()) {
-            result += positions.get(i).positionPrice;
-            i++;
-        }
-        return result;
-    }
-    public void printPosition() {
-        double d = Math.floor(this.positionPrice);
-        switch ((int) (d % 10)) {
-            case 1: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рубль"); break;
-            case 2: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рубля"); break;
-            case 3: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рубля"); break;
-            case 4: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рубля"); break;
-            case 5: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рублей"); break;
-            case 6: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рублей"); break;
-            case 7: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рублей"); break;
-            case 8: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рублей"); break;
-            case 9: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рублей"); break;
-            case 0: System.out.println(this.positionName + ": " + String.format("%.2f",this.positionPrice) + " рублей"); break;
-        }
-    };
-}
-
 public class Main {
 
-    public static ArrayList<Position> counter() {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        ArrayList<Position> positions = new ArrayList();
-        int i = 0;
-        double price = -1;
-        String close = "";
-        String name = "";
-        final Scanner scanner = new Scanner(System.in);
-        PrintStream ps = new PrintStream(System.out); //коммент для коммита
-        do {
-            ps.println("Введите название товара и цену через пробел (дробные цены вводите через ,)");
-            name = scanner.next();
-            price = scanner.nextDouble();
-            while (price <= 0) {
-                ps.println("Введите положительную цену, имя заново вводить не нужно");
-                price = scanner.nextDouble();
-            }
-            Position buf = new Position(name, price);
-            positions.add(buf);
-            i++;
-            ps.println("Товар успешно добавлен");
-            ps.println("Добавить еще товар или завершить ввод?");
-            close = scanner.next();
-        } while (!(close.equalsIgnoreCase("завершить")));
-        return positions;
-    }
+
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
@@ -75,7 +16,7 @@ public class Main {
             System.out.println("Введите количество персон, более 1:");
             amountPeople = scanner.nextInt();
         }
-        ArrayList<Position> resultPositions = counter();
+        ArrayList<Position> resultPositions = new Position().counter();
         ps.println("Добавленные товары:");
         int j = 0;
          do {
@@ -83,19 +24,14 @@ public class Main {
             j++;
         } while (j < resultPositions.size());
          ps.println();
-         result = Position.calculate(resultPositions) / amountPeople;
+         result = new Position().calculate(resultPositions) / amountPeople;
          ps.print("Каждый должен заплатить: ");
-         switch ((int) (result % 10)) {
+         switch ((int) (result % 20)) {
              case 1: ps.println(String.format("%.2f", result)+" рубль"); break;
              case 2: ps.println(String.format("%.2f", result)+" рубля"); break;
              case 3: ps.println(String.format("%.2f", result)+" рубля"); break;
              case 4: ps.println(String.format("%.2f", result)+" рубля"); break;
-             case 5: ps.println(String.format("%.2f", result)+" рублей"); break;
-             case 6: ps.println(String.format("%.2f", result)+" рублей"); break;
-             case 7: ps.println(String.format("%.2f", result)+" рублей"); break;
-             case 8: ps.println(String.format("%.2f", result)+" рублей"); break;
-             case 9: ps.println(String.format("%.2f", result)+" рублей"); break;
-             case 0: ps.println(String.format("%.2f", result)+" рублей"); break;
+             default: ps.println(String.format("%.2f", result)+" рублей"); break;
          }
     }
 }
