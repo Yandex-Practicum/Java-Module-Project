d6880ba91d814a7dcbf58593c83ef1120a16ab36
Skankhunt68 <leono-dim1@yandex.ru>
2022-08-18 14:46:08 +0300

    18.08 13:50 1 variant;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,28 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир это я!");
+        int peopleCount = getPeopleCount();
+        Calculator calculator = new Calculator(peopleCount);
+        calculator.doCalculate();
     }
-}
+    static int getPeopleCount() {
+        int peopleCount;
+        System.out.println("На сколько человек делим счет?");
+        while (true) {
+            peopleCount = scanner.nextInt();
+            if (peopleCount > 1) {
+                System.out.println("Делим счет на " + peopleCount + "х");
+                return peopleCount;
+            }
+            else if (peopleCount == 1) {
+                System.out.println("Счет не делится на одного посетителя!");
+            }
+            System.out.println("Некорректное значение, попробуйте снова!");
+            }
+        }
+    }
