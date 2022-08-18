8121ffe339ed631f46225102141dc6a863a849ba
maxim <estonmax@gmail.com>
2022-08-18 10:37:24 +0300

    1) Создал класс Calculate, отвечающий за расчеты. 2) Добавил бесконечный цикл для корректного ввода кол-ва гостей

---
+++ src/main/java/Calculate.java
@@ -0,0 +1,12 @@
+public class Calculate {
+    int count;
+
+    public Calculate(int count) {
+        this.count = count;
+    }
+
+    public double check(){
+        // делим счет на count и возвращаем это значение
+        return 0;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        Scanner scanner = new Scanner(System.in);
+
+        int countUsers = scanner.nextInt();
+
+        while(true){
+            if (countUsers < 1){
+                System.out.println("Введите корректное число - 1 или больше");
+            } else if (countUsers == 1){
+                break;
+            } else{
+                Calculate calculate = new Calculate(countUsers);
+                calculate.check();
+                break;
+            }
+        }
     }
 }
