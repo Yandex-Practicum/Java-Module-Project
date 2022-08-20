06809bfad53b99b131e9ca149b7263686fd418cc
kotzimaru <kotzimaru@yandex.ru>
2022-08-20 13:48:33 +0300

    Release 2

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,14 +1,16 @@
 public class Product {
     String name;
     double price;
-    Product(String name, double price){
+
+    Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    public void info(){
-        System.out.println("Товар "+ name + " цена "+ price + " " + GetRubleAddition(price));
+    public void info() {
+        System.out.println("Товар " + name + " цена " + price + " " + GetRubleAddition(price));
     }
+
     public static String GetRubleAddition(Double value) {
 
         int num = value.intValue();
@@ -18,12 +20,16 @@ public class Product {
             return "рублей";
         }
 
-        return switch (num % 10) {
-            case 1 -> "рубль";
-            case 2, 3, 4 -> "рубля";
-            default -> "рублей";
-        };
+        switch (num % 10) {
+            case 1:
+                return "рубль";
+            case 2:
+            case 3:
+            case 4:
+                return "рубля";
+            default:
+                return "рублей";
+        }
     }
-
 }
 
