a420c1447d30073eb3daf6311c455314b5f4fc51
biornes <dimaemel1234@gmail.com>
2022-08-21 16:56:54 +0300

    fix merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         // ваш код начнется здесь
-<<<<<<< HEAD
-=======
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
         Calculator calculator = new Calculator();
         calculator.howManyUsers();
         calculator.enterProductAndPrice();
         calculator.showResults();
->>>>>>> 7ace9a2c29c026c50a10e8410eca06b15e35fcf3
     }
 }
