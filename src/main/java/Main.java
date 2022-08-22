2a60157f40d77b47aa9888b2bce1245c6bb58dc7
igor <igor.sheplyakov@yandex.ru>
2022-08-22 16:43:33 +0300

    выполнение задания

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,44 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        System.out.println("Привет, на скольких человек необходимо разделить счёт?");
+        int peopleCount = getCountOfPeople();
+        ProductCalculator productCalculator = new ProductCalculator(peopleCount);
+        productCalculator.inputProducts();
+    }
+
+    public static int getCountOfPeople() {
+        int peopleCount = 0;
+
+        while (true) {
+            try {
+                peopleCount = scanner.nextInt();
+
+                if (isCorrectPeopleCount(peopleCount)) {
+                    break;
+                }
+            } catch (InputMismatchException er) {
+                System.out.println("Некоректное значение");
+                break;
+            }
+        }
+        return peopleCount;
+    }
+
+    public static boolean isCorrectPeopleCount(int peopleCount) {
+        if (peopleCount <= 0) {
+            System.out.println("Количество человек меньше 1. Это некорректное значение для подсчёта. Введите новое значение.");
+            return false;
+        } else if (peopleCount == 1) {
+            System.out.println("Количество человек, введённых пользователем равно 1. В этом случае нет смысла ничего считать и делить. Введите новое значение.");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
---
+++ src/main/java/Product.java
@@ -0,0 +1,17 @@
+public class Product {
+    private final String name;
+    private final float price;
+
+    Product(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+}
