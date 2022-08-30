b96f6e6f94d79f909e393e151186c2f35556f1a8
Lack1t <Lackit27@yandex.ru>
2022-08-30 14:45:18 +0300

    Первая проектная работа

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Calculator {
+        Scanner scanner = new Scanner(System.in);
+        String productList = ("Добавленные товары:");
+        double cost = 0;
+        boolean rightness = false;
+    public void counting() {
+        while (!rightness) {
+            System.out.println("Для завершения списка введите: Завершить" + '\n' + "Введите название продукта");
+            String product;
+            product = scanner.next();
+            if (product.equalsIgnoreCase("Завершить")) {
+                rightness = true;
+            } else {
+                while (true) {
+                    System.out.println("Введите стоимость продукта в формате 'рубли.копейки' [10.45, 11.40]");
+                    if (scanner.hasNextDouble()) {
+                        double price = scanner.nextDouble();
+                        if (price <= 0.0) {
+                            System.out.println("Некорректное значение");
+                        } else {
+                            cost = cost + price;
+                            String worth = String.format("%.2f", price);
+                            productList = productList + '\n' + product + " " + worth;
+                            String list = String.format("Продукт " + product + " %.2f успешно добавлен", price);
+                            System.out.println(list);
+                            break;
+                        }
+                    } else {
+                        System.out.println("Некорректное значение");
+                        scanner.next();
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        int people;
+        String rubles;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Сколько Вас человек");
+            if (scanner.hasNextInt()) {
+                people = scanner.nextInt();
+                if (people == 1) {
+                    System.out.println("Нет смысла считать, для одного человека");
+                }
+                if (people <= 0){
+                    System.out.println("Некорректное значение");
+                }
+                if (people > 1) {
+                    break;
+                }
+            }
+            else {
+                System.out.println("Некорректное значение");
+            }
+
+        }
+
+        Calculator calculate = new Calculator();
+        calculate.counting();
+
+            if (Math.floor(calculate.cost / people) == 1) {
+                rubles = "рубль";
+            }
+            else if (calculate.cost / people >= 2 && calculate.cost / people < 5) {
+                rubles = "рубля";
+
+            } else {
+                rubles = "рублей";
+
+        }
+        String str = String.format(calculate.productList + '\n' + "Итоговая стоимость всех продуктов %.2f" + '\n' + "Каждый заплатит %.2f " + rubles, calculate.cost, calculate.cost/people );
+        System.out.println(str);
+
     }
+
 }
