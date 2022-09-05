4aae70c891fae2943e4d01deaea04faa6e15fdc7
d.rakhmayev <d.rakhmayev@space307.com>
2022-09-05 16:39:32 +0300

    practical work - Denis Rakhmaev

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        System.out.println("На какое количество людей должен быть разделен счет?");
+        int count = scanner.nextInt();
+        while (count < 1) {
+            System.out.println("Неверное количество людей.");
+            count = scanner.nextInt();
+        }
+
+        calculator.calculate(count);
     }
 }
---
+++ src/main/java/Order.java
@@ -0,0 +1,12 @@
+
+
+public class Order {
+    String name;
+    Double price;
+
+    public Order(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
