c1d2db910e0ccf05b9f07f54feb355b051a26b16
KTDem <ktdemchuk@yandex.com>
2022-09-12 22:49:49 +0300

    CProject

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        int i;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Доброго дня!\nНа скольких человек необходимо разделить счёт?");
+
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("Ошибка, введите число. ");
+
+        }
+
+        while (true) {
+            i = scanner.nextInt();
+            if (i <= 1) {
+                System.out.println("Введите корректное количество гостей.");
+            }
+
+            if (i > 1) {
+                System.out.println();
+                break;
+            }
+        }
+
+
+        Calculator calculator = new Calculator();
+        calculator.addDishes();
+        calculator.showResult(i);
+
+
     }
 }
+
+
+
+
