217b08c1ac3f4c78b5f0bbe5d948cfe1839ebda6
Алексей <telenkov@it-impuls.ru>
2022-09-13 17:47:27 +0500

    Внес изменения, на повторную проверку.

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -1,48 +1,59 @@
-
 import java.util.Scanner;
 
 public class Calculate {
 
-    public static int product(double person) {
+    public static void product(double person) {
 
         String food = "";
         double price = 0.0;
-        String itog = "";
 
         while (true) {
             System.out.println("Для добавления товара введите название продукта\nДля завершения программы введите \"Завершить\"");
             Scanner scanner = new Scanner(System.in);
-
             String inputFood = scanner.next();
 
-            if (inputFood.equals("Завершить")) {
 
-                itog = String.format("%.2f", price / person);
-                System.out.println("Покупка завершена, " + String.format("%.0f", person) + " персоны заплатят по " + itog + " рублей");
+            double itog = 0;
+            if (inputFood.equalsIgnoreCase("Завершить")) {
+                itog = (int) (price / person);
+                System.out.println("Покупка завершена, " + String.format("%.0f", person) + " персоны заплатят по " + itog + " " + Matsh.floover(itog));
+                break;
 
             } else {
                 System.out.println("Введите стоимость продукта в формате \"'рубли.копейки' [10.45, 11.40]\"");
                 float inputPrice = (float) scanner.nextDouble();
 
                 food = food + "\n" + inputFood;
+
                 price = price + inputPrice;
-                System.out.println("Вы успешно добавили продукт:" + food + "\nИтог: " + String.format("%.2f", price) + " рублей");
 
-
+                System.out.println("Вы успешно добавили продукт:" + food + "\nИтог: " + String.format("%.2f", price) + " " + Matsh.floover(itog));
             }
         }
     }
+
 }
+class Matsh {
 
-
-
-
-
-
-
-
-
+    public static String floover(double itog){
         
+        int amount = (int) itog;
 
+        if (amount > 100)
+            amount %= 100;
 
+        if (amount > 20)
+            amount %= 10;
 
+        switch (amount) {
+            case 1:
+                return "Рублю";
+            case 2:
+            case 3:
+            case 4:
+                return "Рубля";
+            default:
+                return "Рублей";
+        }
+    }
+}
