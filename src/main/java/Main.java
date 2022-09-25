fce7be0ba55d5837eda76809664f830d47087230
nikopolo <bolliboo@yandex.ru>
2022-09-25 18:26:01 +0200

    добавление нового класса Assistant для читаемости кода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,68 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("На скольких человек необходимо разделить счёт?");
-        Scanner scanner = new Scanner(System.in);
-        //количество человек
-        int numberOfPeople;
-        while (true) {
-            if (scanner.hasNextInt()) {
-                numberOfPeople = scanner.nextInt();
-                if (numberOfPeople > 1) {
-                    break;
-                } else {
-                    System.out.println("Ошибка. Введите корректное количество гостей.");
-                }
-            } else {
-                System.out.println("Ошибка. Введите число.");
-                scanner.next();
-            }
-        }
+        // вспомогательный объект для посчета посетителей и работы с товарами
+        Assistant assistant = new Assistant();
+        // подсчет количество человек
+        assistant.calculatePersons();
 
         // создание калькулятора
         Calculator calculator = new Calculator();
 
         // добавление товара и его стоимости в калькулятор
-        while (true) {
-            // товар
-            System.out.println("Введите название товара:");
-            String product = scanner.next();
-            // стоимость
-            System.out.println("Введите стоимость товара:");
-            double price;
-            if (scanner.hasNextDouble()) {
-                price = scanner.nextDouble();
-                calculator.setProducts(product);
-                calculator.setTotalPrice(price);
-                System.out.println("Товар успешно добавлен.");
-                System.out.println("Добавить ещё один товар?");
-                String addOne = scanner.next();
-                if (addOne.equalsIgnoreCase("Завершить")) {
-                    break;
-                }
-            } else {
-                System.out.println("Ошибка. Товар не добавлен. Введите корректное значение стоимости товара.");
-                scanner.next();
-            }
-        }
+        assistant.addProduct(calculator);
 
         // вывод результатов
-        System.out.print("Добавленные товары:");
-        // вывод товаров
-        System.out.println(calculator.getProducts());
-        // счет каждого посетителя
-        double scoreVisitor = calculator.getTotalPrice() / numberOfPeople;
-        // склонение в зависимости от целочисленной части счета
-        String currency = "";
-        if (Math.floor(scoreVisitor) == 1) {
-            currency = "рубль";
-        } else if (Math.floor(scoreVisitor) > 1 && Math.floor(scoreVisitor) <= 4) {
-            currency = "рубля";
-        } else if (Math.floor(scoreVisitor) > 4) {
-            currency = "рублей";
-        }
-        // вывод счета
-        String formatAnswer = String.format("%.2f %s", scoreVisitor, currency);
-        System.out.println(formatAnswer);
+        assistant.outputResult(calculator);
     }
 }
