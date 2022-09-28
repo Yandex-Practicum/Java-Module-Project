a2ea2491dd486b9f59efb745d08580720f064c1d
smirnofdn <smirnofdn@gmail.com>
2022-09-28 14:44:06 +0300

    add Project 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        int friends = numberOfPersons();
+    }
+
+    static int numberOfPersons() {
+        Calculator calculator = new Calculator();
+        System.out.println("На скольких человек необходимо разделить счёт:");
+        int friends = 0;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                friends = scanner.nextInt();
+            } else {
+                scanner.nextInt();
+            }
+            if (friends > 1) {
+                System.out.println("Делим счет на " + friends + "х");
+                calculator.addProduct();
+                calculator.addOutput(friends);
+                break;
+            } else if (friends == 1) {
+                System.out.println("Это некорректное значение для подсчёта. Нужно больше 1:");
+            } else
+                System.out.println("Это некорректное значение . Нужно больше число:");
+        }
+        return friends;
     }
 }
