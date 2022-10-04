e315bde47609a4191fe814189802e2b6f685db5c
kukidevs <agarev.fedya@yandex.com>
2022-10-04 13:34:30 +0300

    Committed project 1.5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,13 +9,10 @@ public class Main {
         float each = total/guestsCount;
         int intEach = (int) each;
         String formattedEach = String.format("%.2f", each);
-        if(intEach >= 10 && intEach <= 20) {
-            printed(formattedEach, "ей");
-        }
-        else if(intEach%10 == 1){
+        if(intEach%10 == 1 && intEach != 11){
             printed(formattedEach, "ю");
         }
-        else if(intEach%10 > 1 && intEach%10 < 5){
+        else if(intEach%10 > 1 && intEach%10 < 5 && (11 >intEach || intEach > 15)){
             printed(formattedEach, "я");
         }
         else{
@@ -28,65 +25,64 @@ public class Main {
     }
 
     public static float addProducts(){
-        float total;
-        Scanner scanner = new Scanner(System.in);
-
         List<Product>products = new ArrayList<Product>();
         while (true){
-            System.out.println("Введите название товара или напишите \"Завершить\"");
-            String productName = "";
-                    String productName1 = scanner.nextLine();
-            if(products.size() != 0) {
-                 productName = scanner.nextLine();
-            }
+            String productName = nameInput(products);
             if(productName.equalsIgnoreCase("Завершить")){
                 break;
             }
-            System.out.println("Теперь введите цену товара");
-            double price; //любое отрицательное значение подойдет :)
-            while (true){
-                String input = scanner.next();
-                try{
-                    price = Double.parseDouble(input);
-                    if(price < 0) {
-                        System.out.println("Воу-воу, у вас там деньги дают за еду? Позовите меня! А пока вы это делаете, попрошу вас ввести еще раз!");
-                    }
-                    else{
-                        break;
-                    }
-                }
-                catch (Exception ParseException) {
-                    System.out.println("Пожалуйта, введите цену в виде числа");
-                }
-            }
+            double price = priceInput();
             String priceFormatted = String.format("%.2f", price);
-            Product newProd = new Product("Abc", 0);
-            if(products.size() != 0){
-                newProd = new Product(productName, Float.parseFloat(priceFormatted));
-            }
-            else {
-                newProd = new Product(productName1, Float.parseFloat(priceFormatted));
-            }
-
+            Product newProd;
+            newProd = new Product(productName, Float.parseFloat(priceFormatted));
             products.add(newProd);
             System.out.println("Товар успешно добавлен!");
-            total = 0;
+            currentSummary(products, false);
+        }
+        return currentSummary(products, true);}
+
+    public static float currentSummary(List<Product> products, boolean printProducts){
+        float total = 0;
         for(int i = 0; i < products.size(); i++){
-
-            total += products.get(i).price;
-        }
-            System.out.println("Текущая общая сумма - " + total);
-        }
-        total = 0;
-        for(int i = 0; i < products.size(); i++) {
-            System.out.println(products.get(i).name);
-            total += products.get(i).price;
-        }
-            System.out.println("Текущая общая сумма - " + total);
-
+            if(printProducts) {
+                System.out.println(products.get(i).name);
+            }
+            total += products.get(i).price;}
+        System.out.println("Текущая общая сумма - " + total);
         return total;
+    }
+
+    public static String nameInput(List<Product> products){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название товара или напишите \"Завершить\"");
+        String productName = "";
+
+        if(products.size() != 0) {
+            productName = scanner.nextLine();
+            return productName;
+        }else {
+            return scanner.nextLine();
+        }
 
     }
+
+    public static double priceInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Теперь введите цену товара");
+        double price;
+        while (true){
+            String input = scanner.next();
+            try{
+                price = Double.parseDouble(input);
+                if(price < 0) {
+                    System.out.println("Воу-воу, у вас там деньги дают за еду? Позовите меня! А пока вы это делаете, попрошу вас ввести еще раз!");
+                }
+                else{
+                    return price;
+                }
+            }
+            catch (Exception ParseException) {
+                System.out.println("Пожалуйта, введите цену в виде числа");}}}
 
     public static int guestsCount() {
         Scanner scanner = new Scanner(System.in);
