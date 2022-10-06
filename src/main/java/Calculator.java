1c3b394b5cd07c49b9160b9ae4dfefeeff81f199
GoldAlbatross <snovaodin@gmail.com>
2022-10-06 18:25:23 +0300

    first try

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,13 @@
 public class Calculator {
 
-    String productList;
-    double sum;
+    String productList = "";
+    double sum = 0;
 
+    void getSum(double price) {
+        sum = sum + price;
+    }
+
+    void getProductList(String product) {
+        productList = productList + "\n" + product;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,103 @@
+/******************************************************************************
+
+ Online Java Compiler.
+ Code, Compile, Run and Debug java program online.
+ Write your code in this editor and press "Run" button to execute it.
+
+ *******************************************************************************/
 
 import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scan;
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        // Обработка ввода количества гостей пользователем.
+        // Обработка ввода количества гостей пользователем
         System.out.println("How many persons?");
-        int person = 0;
-        int min = 1;
-        while (true) {
-            if (getInt(min) > min) {
-                break;
-            }
-        }
+        int person = getInt(1); // кол-во гостей (аргумент) не должно быть менее 1
 
-        // Обработка ввода названия товара пользователем.
-        System.out.println("Product name:");
+        // Обработка ввода названия товара и цены пользователем
+        Calculator calculator = new Calculator();
+        String end;
+        do {
+            System.out.println("Product name:");
+            String product = getText();
+
+            System.out.println("Price in format(10,45):");
+            double price = getDouble(0.0); // цена (аргумент) не должна быть менее 0.0
+
+            calculator.getProductList(product);
+            calculator.getSum(price);
+
+            System.out.println("Enough? (y/n)");
+            end = getText();
+        } while (!end.equalsIgnoreCase("y"));
+
+        // Вывод результата
+        System.out.println("ProductList:" + calculator.productList);
+        double personalBill = calculator.sum / person;
+        String rub = getRub(personalBill);
+        System.out.printf(" %.2f %s from each!", personalBill, rub);
     }
 
 
-    public static int getInt(int min) {
-        int a = 0;
-        scan = new Scanner(System.in);
-        if (!scan.hasNextInt()) {
-            System.out.println("this is not a number, try again.");
-        } else if (scan.hasNextInt()) {
-            a = scan.nextInt();
-            if (a <= min) {
-                System.out.println("this is not a valid value, try again.");
+    public static int getInt(int minPerson) {
+        int person;
+        while (true) {
+            if (scan.hasNextInt()) {
+                person = scan.nextInt();
+                if (person > minPerson) break;
+                else System.out.println("This is not a valid value, try again.");
+            } else {
+                System.out.println("This is not a valid value, try again.");
+                scan.next();
             }
         }
-        return a;
+        return person;
+    }
+
+
+    public static String getText() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+
+    public static double getDouble(double minPrice) {
+        double price;
+        while (true) {
+            if (scan.hasNextDouble()) {
+                price = scan.nextDouble();
+                if (price > minPrice) break;
+                else System.out.println("This is not a valid value of price, try again.");
+            } else {
+                System.out.println("This is not a valid value of price, try again.");
+                scan.next();
+            }
+        }
+        return price;
+    }
+
+
+    public static String getRub(double personalBill) {
+        String rub = "0" + (int)personalBill;
+        if (rub.endsWith("1")) {
+            if(!rub.substring(0, rub.length() - 1).endsWith("1")) {
+                rub = "rubl'";
+            } else rub = "rubley";
+
+        } else if (rub.endsWith("2") || rub.endsWith("3") || rub.endsWith("4")) {
+            if(!rub.substring(0, rub.length() - 1).endsWith("1")) {
+                rub = "rublya";
+            } else rub = "rubley";
+
+        } else rub = "rubley";
+        return rub;
     }
 }
+
 
 
 
