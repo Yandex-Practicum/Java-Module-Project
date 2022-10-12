aa56515eb6a0aa59d2fdb3ed84b54f0212f24b51
kamberbattch <ak79rus@yandex.ru>
2022-10-12 14:52:49 +1000

    Итог 12.10.22

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,59 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    private static final Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        Calculator calc = new Calculator();
+        calc.inputPersons();
+        calc.addProducts();
+        calc.payPeople();
+    }
+
+    public static class Calculator {
+        double totalPrice = 0;
+        String names = "";
+        int persons = 0;
+
+        public void inputPersons() {
+            while (true) {
+                System.out.println("How many persons?");
+                persons = scan.nextInt();
+                if (persons <= 1) {
+                    System.out.println("Amount of persons must be > 1. Try again.");
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        public void addProducts() {
+            while (true) {
+                System.out.println("Product name: ");
+                String name = scan.next() + scan.nextLine();
+                System.out.println("Price: ");
+                if (scan.hasNextDouble()) {
+                    double price = scan.nextDouble();
+                    totalPrice += price;
+                    names += name + "\n";
+                } else {
+                    System.out.println("Price must be double! Try again");
+                    scan.nextLine();
+                    continue;
+                }
+
+                System.out.println("Product " + name + " successfully added! Enough? (yes - any symbol/no - n): ");
+                String answer = scan.next();
+                if (answer.equalsIgnoreCase("n")) {
+                    continue;
+                } else {break;}
+            }
+        }
+
+        public void payPeople() {
+            System.out.println("You ordered following products:\n" + names + "for total price " + totalPrice);
+            String result = String.format("%.2f", totalPrice/persons);
+            System.out.println(String.format("Each of the " + persons + " people must pay for " + result));
+        }
     }
 }
