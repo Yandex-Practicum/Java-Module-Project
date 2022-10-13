3f6996a9261fdabf9431273369269d1cb201b3f3
kamberbattch <ak79rus@yandex.ru>
2022-10-13 19:02:31 +1000

    Итог 12.10.22+

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,45 +3,52 @@ import java.util.Scanner;
 public class Main {
     private static final Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        calc.inputPersons();
-        calc.addProducts();
-        calc.payPeople();
+        Calculator.inputPersons();
+        Calculator.addProducts();
+        Calculator.payPeople();
     }
 
     public static class Calculator {
-        double totalPrice = 0;
-        String names = "";
-        int persons = 0;
+        static double totalPrice = 0;
+        static String names = "";
+        static int persons = 0;
 
-        public void inputPersons() {
+        public static void inputPersons() {
             while (true) {
                 System.out.println("How many persons?");
-                persons = scan.nextInt();
-                if (persons <= 1) {
-                    System.out.println("Amount of persons must be > 1. Try again.");
-                    continue;
-                } else {
+                if (scan.hasNextInt()) {
+                    persons = scan.nextInt();
+                    if (persons <= 1) {
+                        System.out.println("Amount of persons must be integer > 1. Try again.");
+                        continue;
+                    }
                     break;
+                } else {
+                    System.out.println("Amount of persons must be integer > 1. Try again.");
+                    scan.nextLine();
+                    continue;
                 }
             }
         }
 
-        public void addProducts() {
+        public static void addProducts() {
             while (true) {
                 System.out.println("Product name: ");
                 String name = scan.next() + scan.nextLine();
                 System.out.println("Price: ");
                 if (scan.hasNextDouble()) {
                     double price = scan.nextDouble();
+                    if (price < 0) {
+                        System.out.println("Price must be positive double! Try again");
+                        continue;
+                    }
                     totalPrice += price;
                     names += name + "\n";
                 } else {
-                    System.out.println("Price must be double! Try again");
+                    System.out.println("Price must be positive double! Try again");
                     scan.nextLine();
                     continue;
                 }
-
                 System.out.println("Product " + name + " successfully added! Enough? (yes - any symbol/no - n): ");
                 String answer = scan.next();
                 if (answer.equalsIgnoreCase("n")) {
@@ -50,10 +57,27 @@ public class Main {
             }
         }
 
-        public void payPeople() {
-            System.out.println("You ordered following products:\n" + names + "for total price " + totalPrice);
-            String result = String.format("%.2f", totalPrice/persons);
-            System.out.println(String.format("Each of the " + persons + " people must pay for " + result));
+        public static void payPeople() {
+            int ruble = (int) Math.floor(totalPrice);
+            int payPeople = (int) Math.floor(totalPrice/persons);
+            System.out.println("Вы заказали следующие продукты:\n" + names + "на сумму: " + totalPrice + " " + Calculator.payPeoples(ruble));
+            System.out.println("Каждый из " + persons + " человек заплатит по " + String.format("%.2f", totalPrice/persons) + " " + Calculator.payPeoples(payPeople));
+        }
+
+        static String payPeoples(int ruble) {
+            if (ruble % 100 / 10 == 1) {
+                return "рублей";
+            } else {
+                switch (ruble % 10) {
+                    case 1 :
+                        return "рубль";
+                    case 2:
+                    case 3:
+                    case 4:
+                        return "рубля";
+                    default: return "рублей";
+                }
+            }
         }
     }
 }
