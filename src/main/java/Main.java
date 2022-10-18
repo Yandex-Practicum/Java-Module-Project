561caf715eb7ad4bd16d6aee8690a5923525b4be
RomanKalin27 <kalinroman034@gmail.com>
2022-10-19 00:42:48 +0300

    Калькулятор. ПР №1.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("На сколько человек вы хотели бы разделить чек?");
-        int numPeople = scan.nextInt();
-        while (1 < 4) {
-            if (numPeople > 1) {
-                break;
+        try {
+            int numPeople = scan.nextInt();
+            while (1 < 4) {
+                if (numPeople > 1) {
+                    break;
+                }
+                System.out.println("Ошибка. Невозможно разделить чек на данное колличество людей. Попробуйте снова.");
+                numPeople = scan.nextInt();
             }
-            System.out.println("Ошибка. Невозможно разделить чек на данное колличество людей. Попробуйте снова.");
-            numPeople = scan.nextInt();
+
+            double check = calc.calculator() / numPeople;
+            System.out.println("Каждый человек должен заплатить: " + check + calc.ending(check));
+        } catch (Exception e){
+            System.out.println("Ошибка. Попробуйте снова.");
         }
-        double check = calc.calculator()/numPeople;
-        System.out.println("Каждый человек должен заплатить: " + check);
     }
 }
--- src/main/java/calc.java
+++ src/main/java/calc.java
@@ -13,34 +13,43 @@ public class calc {
                 System.out.println("Пожалуйста, введите цену товара в формате 'рубли.копейки' [10.45, 11.40]");
                 double itemCost = scan3.nextDouble();
                 System.out.println("Товар добавлен в список.");
-                i = i + "\n" + itemName + " " + itemCost;
+                i = i + "\n" + itemName + " " + itemCost + ending(itemCost);
                 j = j + itemCost;
-                System.out.println("Хотите ли вы завершить покупки?");
+                System.out.println("Хотите ли бы вы завершить покупки?");
                 String answer = scan4.nextLine();
                 String stopWord = "Завершить";
                 if (answer.equalsIgnoreCase(stopWord)) {
                     System.out.println(i);
-                    double summ = Math.floor(j) % 10;
-                    int remainder = (int) summ;
-                    switch (remainder) {
-                        case 1:
-                            System.out.println(String.format("Общая сумма покупок: %.2f рубль", j));
-                            break;
-                        case 2:
-                            System.out.println(String.format("Общая сумма покупок: %.2f рубля", j));
-                            break;
-                        case 3:
-                            System.out.println(String.format("Общая сумма покупок: %.2f рубля", j));
-                            break;
-                        case 4:
-                            System.out.println(String.format("Общая сумма покупок: %.2f рубля", j));
-                            break;
-                        default:
-                            System.out.println(String.format("Общая сумма покупок: %.2f рублей", j));
-                            break;
-                    }
+                    System.out.println("Общая сумма покупок: " + j + ending(j));
                     return j;
                 }
+            }
+        }
+    }
+    public static String ending(double j){
+        if(Math.floor(j) % 100 == 11 ^ Math.floor(j) % 100 == 12 ^ Math.floor(j) % 100 == 13 ^ Math.floor(j) % 100 == 14){
+            String stringe = " рублей";
+            return stringe;
+        }
+        else {
+            double summ = Math.floor(j) % 10;
+            int remainder = (int) summ;
+            switch (remainder) {
+                case 1:
+                    String string1 = " рубль";
+                    return string1;
+                case 2:
+                    String string2 = " рубля";
+                    return string2;
+                case 3:
+                    String string3 = " рубля";
+                    return string3;
+                case 4:
+                    String string4 = " рубля";
+                    return string4;
+                default:
+                    String stringdef = " рублей";
+                    return stringdef;
             }
         }
     }
