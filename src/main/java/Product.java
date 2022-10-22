2dda24e0e2982bdb55dca18d002c7f0dbb8635eb
vladalexeco <vladalexeco86@gmail.com>
2022-10-22 23:25:57 +0300

    Добавление класса Product

---
+++ src/main/java/Product.java
@@ -0,0 +1,23 @@
+public class Product {
+    /* На основе этого класса будут созданы объекты Product, которые будут служить
+    исходным материалом для расчета общей стоимости заказа
+     */
+
+    // наименование товара
+    private final String name;
+    // стоимость товара
+    private final double value;
+
+    public Product(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+}
