de4831ce5c8b2a58176d9037122385ef043f7f64
Timofey <ruplanetcom@gmail.com>
2022-10-23 15:04:30 +0300

    учтены замечания

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,9 +10,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int input;
 
+
+        System.out.println("На скольких человек вы хотите разделить счёт?");
         while (true) {
             try {
-                System.out.println("На скольких человек вы хотите разделить счёт?");
                 input = scanner.nextInt();
                 if (input <= 1) {
                     System.out.println("Введите значение больше 1");
@@ -23,7 +24,6 @@ public class Main {
                 System.out.println("Введите число");
                 scanner.nextLine();
             }
-
         }
         Calculator calc = new Calculator(input);
         calc.add();
@@ -31,76 +31,3 @@ public class Main {
     }
 }
 
-class Calculator {
-    HashMap<String, Double> products = new HashMap<>();
-    int persons;
-    double sum;
-    double payment;
-    private Scanner scanner = new Scanner(System.in);
-    private String input;
-    private Double dInput;
-    Calculator (int persons) {
-        this.persons = persons;
-    }
-
-    void add() {
-        while (true) {
-            System.out.println("Введите название товара: ");
-            input = scanner.nextLine();
-            System.out.println("Введите стоимость товара в формате: рубли.копейки ");
-
-            try {
-                dInput = scanner.nextDouble();
-            }
-            catch (InputMismatchException e) {
-                System.out.println("Введите число");
-                scanner.nextLine();
-                continue;
-            }
-
-            if (dInput < 0) {
-                System.out.println("Введите стоимость больше нуля");
-                scanner.nextLine();
-            }
-            else {
-                products.put(input, dInput);
-                sum += dInput;
-                System.out.println("Товар успешно добавлен!");
-                scanner.nextLine();
-                System.out.println("Хотите добавить ещё товар? Введите любой символ. Иначе, введите \"Завершить\", чтобы закончить ввод.");
-                input = scanner.nextLine();
-                if (input.equalsIgnoreCase("завершить")) {
-                    break;
-                }
-            }
-
-        }
-    }
-
-    void print() {
-        payment = sum / persons;
-        System.out.println("Добавленные товары:");
-        products.forEach((key, value) -> {
-            if (Math.floor(value) % 10 == 2 || Math.floor(value) % 10 == 3 || Math.floor(value) % 10 == 4) {
-                System.out.println(key + " " + value + " рубля");
-            } else if (Math.floor(value) % 10 == 1 && Math.floor(value) % 100 != 11) {
-                System.out.println(key + " " + value + " рубль");
-            } else {
-                System.out.println(key + " " + value + " рублей");
-            }
-        });
-        System.out.println("Каждый должен заплатить:");
-        format(payment);
-
-    }
-
-    void format(double payment) {
-        if (Math.floor(payment) % 10 == 2 || Math.floor(payment) % 10 == 3 || Math.floor(payment) % 10 == 4) {
-            System.out.println(String.format("%.2f", payment) + " рубля");
-        } else if (Math.floor(payment) % 10 == 1 && Math.floor(payment) % 100 != 11) {
-            System.out.println(String.format("%.2f", payment) + " рубль");
-        } else {
-            System.out.println(String.format("%.2f", payment) + " рублей");
-        }
-    }
-}
