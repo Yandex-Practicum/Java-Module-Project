18e82aff952c4c4a5969d51388f7a08e80fd8698
Karakaten <102169385+Karakaten@users.noreply.github.com>
2022-10-24 13:57:24 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        while (true) {
+            System.out.println("На скольких человек разделить счет?");
+            int scanPerson = scanner.nextInt();
+
+            if (scanPerson == 1) {
+                System.out.println("Подсчет не нужен");
+                break;
+            } else if (scanPerson < 1) {
+                System.out.println("Ошибка. Введите корректное значение.");
+            } else if (scanPerson > 1) {
+                System.out.println("Количество человек: " + scanPerson);
+                break;
+            }
+            
+        }
+
+        while (true) {
+            Product product = new Product();
+            System.out.println("Введите название товара");
+            product.setNameOfProduct(scanner.next());
+            System.out.println("Введите стоимость товара в формате 00,00");
+            product.setCost(scanner.nextDouble());
+            System.out.println("Все? Да/Нет");
+            if (scanner.next().equals("Да")) {
+                break;
+            }
+            calculator.setBill(product);
+        }
+
+    calculator.calculate();
     }
 }
-
