61a9d9dcbd15c32075cf3bb0c3bb86320eecbf4d
Karakaten <102169385+Karakaten@users.noreply.github.com>
2022-10-25 12:38:10 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,17 +3,23 @@ public class Calculator {
     String bill = "";
     double finalPrice = 0;
     int countPerson = 0;
+    
 
+    public void setCountPerson(int countPerson) {
+        this.countPerson = countPerson;
+    }
+    
     public void setBill(Product product) {
         bill += (product.getNameOfProduct() + "\n" + product.getCost()) + "\n";
         finalPrice += product.getCost();
-        countPerson = product.getCountPerson();
+
         }
 
 
     public void calculate() {
         double priceOfPerson = finalPrice / countPerson;
-        System.out.println("Общий чек: " + "\n" + bill + "\n" + "Денег с человека:" + "\n" + priceOfPerson  + " рублей");
+        System.out.println("Количество персон: " + countPerson + "\n" + "Общий чек: " +
+                "\n" + bill + "\n" + "Денег с человека:" + "\n" + priceOfPerson  + " рублей");
     }
 
 }
