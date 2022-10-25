066c7c2b3038033e4d925901f460d8ff1e4b6068
Karakaten <102169385+Karakaten@users.noreply.github.com>
2022-10-25 09:46:41 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,37 +5,38 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        Product product = new Product();
+
         while (true) {
+            Product product = new Product();
             System.out.println("На скольких человек разделить счет?");
             int scanPerson = scanner.nextInt();
 
             if (scanPerson == 1) {
                 System.out.println("Подсчет не нужен");
-                break;
+                System.exit(0);
             } else if (scanPerson < 1) {
                 System.out.println("Ошибка. Введите корректное значение.");
             } else if (scanPerson > 1) {
-                System.out.println("Количество человек: " + scanPerson);
                 product.setCountPerson(scanPerson);
-                break;
+                System.out.println("Количество человек: " + scanPerson);
+
             }
+            while (true) {
+
+                System.out.println("Введите название товара");
+                product.setNameOfProduct(scanner.next());
+                System.out.println("Введите стоимость товара в формате 00,00");
+                product.setCost(scanner.nextDouble());
+                calculator.setBill(product);
+                System.out.println("Завершить? Нажмите любой символ, чтобы продолжить");
+                if (scanner.next().equalsIgnoreCase("Завершить")) {
+                    break;
+                }
+
+            }
+            calculator.calculate();
+            System.exit(0);
 
         }
-
-        while (true) {
-
-            System.out.println("Введите название товара");
-            product.setNameOfProduct(scanner.next());
-            System.out.println("Введите стоимость товара в формате 00,00");
-            product.setCost(scanner.nextDouble());
-            System.out.println("Все? Да/Нет");
-            if (scanner.next().equals("Да")) {
-                break;
-            }
-            calculator.setBill(product);
-        }
-
-    calculator.calculate();
     }
 }
