57e717d870498ecf7e610c77d04053ece86a75f4
Djsdriver <alexei.gra4ev@gmail.com>
2022-10-25 11:36:45 +0300

    Практическая работа №1 "Консольное приложение"

---
+++ src/main/java/Const.java
@@ -0,0 +1,5 @@
+public class Const{
+    final String ADD_MESSAGE_PRODUCT="Товар успешно добавлен";
+    final String QUESTION_MESSAGE="Хотите ли вы добавить еще товар, выберите напишите 'Да' или напишите 'Завершить' для выхода";
+    final String ADD_PRODUCT_AND_PRICE="Укажите название товара и его стоимость";
+}
