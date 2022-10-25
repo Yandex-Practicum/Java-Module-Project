5559530037ced8eacca0aa4bf5c7b5eb69bc3a87
psycow <ladybugaga@gmaIl.com>
2022-10-25 19:43:38 +0300

    Версия номер один.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        numberOfGuestsQuestion();
+    }
+     private static int numberOfGuestsQuestion(){
+        int guestAmount = 0;
+        Scanner scanner = new Scanner(System.in);
+                int userInput = 0;
+                while (true){
+                    System.out.println("Введите количество гостей.");
+                    userInput = scanner.nextInt();
+                      if (userInput == 1){
+                          System.out.println("Количество гостей дожно быть не менее 2-х человек. Найдите кого-нибудь, с кем можно разделить счёт.");
+          }else if (userInput <2) {
+                          System.out.println("Количество гостей не может быть меньше 1. Введите корректное значение.");
+                      }else {
+                          guestAmount = userInput;
+                          System.out.println("Количество гостей: " + guestAmount);
+                          return guestAmount;
+                      }
+
+        }
     }
 }
