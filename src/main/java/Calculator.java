3c5d7487c745b788cd96e2bd1abee4bb1257edcf
Karakaten <102169385+Karakaten@users.noreply.github.com>
2022-10-26 15:11:34 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,23 +3,36 @@ public class Calculator {
     String bill = "";
     double finalPrice = 0;
     int countPerson = 0;
-    
+
+    double priceOfPerson = 0;
 
     public void setCountPerson(int countPerson) {
         this.countPerson = countPerson;
     }
-    
+
     public void setBill(Product product) {
-        bill += (product.getNameOfProduct() + "\n" + product.getCost()) + "\n";
+        bill += (product.getNameOfProduct() + " " + product.getCost()) + "\n";
         finalPrice += product.getCost();
 
         }
 
-
-    public void calculate() {
-        double priceOfPerson = finalPrice / countPerson;
-        System.out.println("Количество персон: " + countPerson + "\n" + "Общий чек: " +
-                "\n" + bill + "\n" + "Денег с человека:" + "\n" + priceOfPerson  + " рублей");
+    public String math() {
+        String rubles = "";
+        if (priceOfPerson < 5 || priceOfPerson % 10 > 1) {
+            rubles = "рубля";
+        } else if (priceOfPerson % 10 == 0 || priceOfPerson >= 6) {
+            rubles = "рублей";
+        } else if (priceOfPerson % 10 == 1 || priceOfPerson == 1){
+            rubles = "рубль";
+        }
+        return rubles;
     }
+    public void calculate() {
+        priceOfPerson = finalPrice / countPerson;
+        System.out.println("Количество персон: " + countPerson + "\n" + "Общий чек: " +
+                "\n" + bill + "\n" + "Денег с человека:" + "\n" + String.format("%.2f", priceOfPerson) + " " + math());
+    }
+
+
 
 }
