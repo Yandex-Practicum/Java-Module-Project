27383aa0b46ab805280117b736b96a5848f73b87
Karakaten <102169385+Karakaten@users.noreply.github.com>
2022-10-28 12:56:56 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+
         Calculator calculator = new Calculator();
         System.out.println("На скольких человек разделить счет?");
-        int scanPerson = scanner.nextInt();
-        calculator.setCountPerson(scanPerson);
-        while (true) {
+        int scanPerson;
 
+
+        while (true) {
+            scanPerson = scanner.nextInt();
             if (scanPerson == 1) {
                 System.out.println("Подсчет не нужен");
                 System.exit(0);
             } else if (scanPerson < 1) {
                 System.out.println("Ошибка. Введите корректное значение.");
-                System.exit(0);
-            } else {
-                Product product = new Product();
-                System.out.println("Введите название товара");
-                product.setNameOfProduct(scanner.next());
-                System.out.println("Введите стоимость товара в формате 00,00");
-                product.setCost(scanner.nextDouble());
-                calculator.setBill(product);
-                System.out.println("Завершить? Нажмите любой символ, чтобы продолжить");
-                if (scanner.next().equalsIgnoreCase("Завершить")) {
-                    break;
-            }
+            } else break;
         }
-        }
-            calculator.calculate();
+
+        calculator.setCountPerson(scanPerson);
+        do {
+            Product product = new Product();
+            System.out.println("Введите название товара");
+            scanner.nextLine();
+            product.setNameOfProduct(scanner.nextLine());
+            System.out.println("Введите стоимость товара в формате 00,00");
+            product.setCost(scanner.nextDouble());
+            calculator.setBill(product);
+            System.out.println("Завершить? Нажмите любой символ, чтобы продолжить");
+        } while (!scanner.next().equalsIgnoreCase("Завершить"));
+        calculator.calculate();
 
     }
 }
