4f90a7b036f919d90e810d23e5133dc4a13736b5
harhanov <harhanov@gmail.com>
2022-10-28 16:37:37 +0300

    Проверяет количество гостей, добавляет в список продукты и выводит его.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,27 +2,54 @@ import java.util.Scanner;
 
 public class Calculator {
 
-
         static Scanner scanner = new Scanner(System.in);
-        String userInput;
+        static String userInput = null;
         static String productName;
-        static String allProductsList;
+        static String allProductsList = "Добавленные товары:";
         static float productCost = 0.0F;
         static float allProductsCost = 0.0F;
+        static String checkedNumber = null;
 
-    public static void addingProducts() {
-            String userInput = null;
-            while (userInput.toUpperCase() != "ЗАВЕРШИТЬ"){
+        static void printResult(int numberOfGuests) {
+            while (!"ЗАВЕРШИТЬ".equalsIgnoreCase(userInput)){
                 System.out.println("Введите название товара и его стоимость.\nТовар: ");
                 productName = scanner.next();
-                allProductsList = allProductsList + " " + productName;
+                allProductsList = allProductsList + "\n" + productName;
                 System.out.println("Введите стоимость: ");
-                productCost = scanner.nextFloat();
-                allProductsCost = allProductsCost + productCost;
+                checkedNumber = scanner.next();
 
-                System.out.println("Вы успешно добавили" + productName + "за" + productCost + "рублей.\nХотите добавить ещё один товар?");
+                while (true){
+                    if (!isItaNumber(checkedNumber)){
+                        System.out.println("Бип, ошибка. Это не число, либо вы в качестве делителя используете запятую вместо точки.\nПопробуйте ещё раз. Введите стоимость: ");
+                        checkedNumber = scanner.next();
+                    }else if (!isItPositive(productCost = Float.parseFloat(checkedNumber))){
+                        System.out.println("Бип, ошибка. Стоимость не может быть отрицательной!\nПопробуйте ещё раз. Введите стоимость: ");
+                        checkedNumber = scanner.next();
+                    }else{
+
+                        break;
+                    }
+                }
+
+                allProductsCost += productCost;
+                System.out.println("Вы успешно добавили " + productName + " за " + productCost + " рублей.\nЕсли хотите завершить, наберите \"Завершить\" и нажмите \"enter\".\nЕсли хотите добавить ещё один товар, введите любой символ или слово и нажмите \"enter\".");
                 userInput = scanner.next();
             }
-        }
-    }
+            System.out.println(allProductsList + "\nОбщая сумма: " + allProductsCost+ " рублей.");
 
+        }
+
+        private static boolean isItPositive(float productCost) {
+            if (productCost>=0) {
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        private static boolean isItaNumber(String checkedNumber) {
+
+            return checkedNumber.matches("-?\\d+(\\.\\d+)?");
+        }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
 
-
 public class Main {
 
     public static void main(String[] args) {
+        int numberOfGuests = 0;
+        numberOfGuestsQuestion();
+        Calculator.printResult(numberOfGuests);
 
-        //int guestAmount = numberOfGuestsQuestion();
-        //System.out.println("Количество гостей: " + guestAmount);
-        Calculator calculator = new Calculator();
-        calculator.addingProducts();
+        //Calculator calculator = new Calculator();
     }
-     private static int numberOfGuestsQuestion(){
+    static int numberOfGuestsQuestion(){
         Scanner scanner = new Scanner(System.in);
-                int userInput = 0;
+                int numberOfGuests = 0;
                 while (true){
                     System.out.println("Введите количество гостей.");
-                    userInput = scanner.nextInt();
-                      if (userInput == 1){
+                    numberOfGuests = scanner.nextInt();
+                      if (numberOfGuests == 1){
                           System.out.println("Количество гостей дожно быть не менее 2-х человек. Найдите кого-нибудь, с кем можно разделить счёт.");
-          }else if (userInput <2) {
-                          System.out.println("Количество гостей не может быть меньше 1. Оглядитесь в поисках своих друзей.");
+          }else if (numberOfGuests <2) {
+                          System.out.println("Количество гостей не может быть меньше 1. Вероятно вы ошиблись.");
                       }else {
-                          return userInput;
+                          return numberOfGuests;
                       }
 
         }
