57deb096b6097d750e2bf05fe5a3632600520bdc
psycow <ladybugaga@gmaIl.com>
2022-10-29 12:55:42 +0300

    Версия номер 1.01

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,58 +2,59 @@ import java.util.Scanner;
 
 public class Calculator {
 
-        static Scanner scanner = new Scanner(System.in);
-        static String userInput = null;
-        static String productName;
-        static String allProductsList = "Добавленные товары:";
-        static float productCost = 0.0F;
-        static float allProductsCost = 0.0F;
-        static float personalAmount = 0.0F;
-        static String checkedNumber = null;
+    static Scanner scanner = new Scanner(System.in);
+    static String userInput = null;
+    static String productName;
+    static String allProductsList = "Добавленные товары:";
+    static float productCost = 0.0F;
+    static float allProductsCost = 0.0F;
+    static float personalAmount = 0.0F;
+    static String checkedNumber = null;
 
-        static void printResult(int numberOfGuests) {
-            while (!"ЗАВЕРШИТЬ".equalsIgnoreCase(userInput)){
-                System.out.println("Введите название товара и его цену.\nТовар: ");
-                productName = scanner.next();
-                allProductsList = allProductsList + "\n" + productName;
-                System.out.println("Введите стоимость: ");
-                checkedNumber = scanner.next();
+    static void printResult(int numberOfGuests) {
+        while (!"ЗАВЕРШИТЬ".equalsIgnoreCase(userInput)) {// цикл продолжается до ввода слова "заврешить" игнорируя регистр букв
+            System.out.println("Введите название товара и его цену.\nТовар: ");
+            productName = scanner.next();
+            allProductsList = allProductsList + "\n" + productName;
+            System.out.println("Введите стоимость: ");
+            checkedNumber = scanner.next();
 
-                while (true){
-                    if (!isItaNumber(checkedNumber)){
-                        System.out.println("Бип, ошибка. Это не число, либо вы в качестве делителя используете запятую вместо точки.\nПопробуйте ещё раз. Введите цену: ");
-                        checkedNumber = scanner.next();
-                    }else if (!isItPositive(productCost = Float.parseFloat(checkedNumber))){
-                        System.out.println("Бип, ошибка. Цена не может быть отрицательной!\nПопробуйте ещё раз. Введите цену: ");
-                        checkedNumber = scanner.next();
-                    }else{
+            while (true) {// цикл проверяет число ли ввёл пользователь и не является ли оно отрицательным
+                if (!isItaNumber(checkedNumber)) {
+                    System.out.println("Бип, ошибка. Это не число, либо вы в качестве делителя используете запятую вместо точки.\nПопробуйте ещё раз. Введите цену: ");
+                    checkedNumber = scanner.next();
+                } else if (!isItPositive(productCost = Float.parseFloat(checkedNumber))) {
+                    System.out.println("Бип, ошибка. Цена не может быть отрицательной!\nПопробуйте ещё раз. Введите цену: ");
+                    checkedNumber = scanner.next();
+                } else {
 
-                        break;
-                    }
+                    break;
                 }
-
-                allProductsCost += productCost;
-                System.out.println("Вы успешно добавили " + productName + " за " + String.format("%.2f", productCost) + " рублей.\nЕсли хотите завершить, наберите \"Завершить\" и нажмите \"enter\".\nЕсли хотите добавить ещё один товар, введите любой символ или слово и нажмите \"enter\".");
-                userInput = scanner.next();
             }
-            String rubleEnding = getRubleEnding((int) Math.floor(allProductsCost));
-            System.out.println(allProductsList + "\nОбщая сумма: " + String.format("%.2f",allProductsCost) + rubleEnding);
-            personalAmount = allProductsCost / numberOfGuests;
-            String personalRubleEnding = getRubleEnding((int) Math.floor(personalAmount));
-            System.out.println("Каждому гостю необходимо заплатить: " + String.format("%.2f", personalAmount) + personalRubleEnding);
 
+            allProductsCost += productCost;
+            System.out.println("Вы успешно добавили " + productName + " за " + String.format("%.2f", productCost) + " рублей.\nЕсли хотите завершить, наберите \"Завершить\" и нажмите \"enter\".\nЕсли хотите добавить ещё один товар, введите любой символ или слово и нажмите \"enter\".");
+            userInput = scanner.next();
         }
+        String rubleEnding = getRubleEnding((int) Math.floor(allProductsCost)); // выясняем верное окончание слова "рубль" для общей суммы заказа
+        System.out.println(allProductsList + "\nОбщая сумма: " + String.format("%.2f", allProductsCost) + rubleEnding);
+        personalAmount = allProductsCost / numberOfGuests;
+        String personalRubleEnding = getRubleEnding((int) Math.floor(personalAmount));// выясняем верное окончание слова "рубль" для суммы отдельного гостя
+        System.out.println("Каждому гостю необходимо заплатить: " + String.format("%.2f", personalAmount) + personalRubleEnding);
+
+    }
 
 
-        private static boolean isItPositive(float productCost) {// проверяем не является ли число отрицательным
-            return productCost >= 0;
-        }
+    private static boolean isItPositive(float productCost) {// проверяем не является ли число отрицательным
+        return productCost >= 0;
+    }
 
-        private static boolean isItaNumber(String checkedNumber) {// проверяем число ли ввёл пользователь
+    private static boolean isItaNumber(String checkedNumber) {// проверяем число ли ввёл пользователь
 
-            return checkedNumber.matches("-?\\d+(\\.\\d+)?");
-        }
-    public static String getRubleEnding(int wholeRubles) {
+        return checkedNumber.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    public static String getRubleEnding(int wholeRubles) {// подбираем правльное окончание слову "рубль"
         int preLastDigit = wholeRubles % 100 / 10;
         if (preLastDigit == 1) {
             return " рублей.";
@@ -67,8 +68,8 @@ public class Calculator {
                 case 4:
                     return " рубля.";
                 default:
-                    return  " рублей.";
+                    return " рублей.";
             }
+        }
     }
-}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,19 @@ public class Main {
         Calculator.printResult(numberOfGuests);
 
     }
-    static int getNumberOfGuests(){
+
+    static int getNumberOfGuests() {
         Scanner scanner = new Scanner(System.in);
-                while (true){
-                    System.out.println("Введите количество гостей.");
-                    int number = scanner.nextInt();
-                      if (number == 1){
-                          System.out.println("Количество гостей дожно быть не менее 2-х человек. Найдите кого-нибудь, с кем можно разделить счёт.");
-          }else if (number <2) {
-                          System.out.println("Количество гостей не может быть меньше 1. Вероятно вы ошиблись.");
-                      }else {
-                          return number;
-                      }
+        while (true) {
+            System.out.println("Введите количество гостей.");
+            int number = scanner.nextInt();
+            if (number == 1) {
+                System.out.println("Количество гостей дожно быть не менее 2-х человек. Найдите кого-нибудь, с кем можно разделить счёт.");
+            } else if (number < 2) {
+                System.out.println("Количество гостей не может быть меньше 1. Вероятно вы ошиблись.");
+            } else {
+                return number;
+            }
 
         }
     }
