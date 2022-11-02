10bdf8c314ee0a05ec3cbe6966d1c59708a9df04
manualcheg <manualcheg@yandex.ru>
2022-11-02 10:07:10 +0300

    Final version. Cosmetic changes2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        int numberOfFriends;
+
+        System.out.println("Программа \"Калькулятор счёта\"");
+        numberOfFriends = inputOfFriends();
+        calc.input(numberOfFriends);
     }
+
+
+
+    public static int inputOfFriends() {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfFriends;
+        do {
+            System.out.println("Введите количество друзей: ");
+            while (!scanner.hasNextInt()){
+                System.out.println("Вы ввели не целое число! Повторите ввод числа друзей:");
+                scanner.next();
+            }
+            numberOfFriends = scanner.nextInt();
+            if (numberOfFriends <= 0) {
+                System.out.println("Число друзей не может быть равным нулю или быть отрицательным!");
+            } else if (numberOfFriends == 1) {
+                System.out.println("Число друзей должно быть больше 1 !");
+            }
+        }
+        while (numberOfFriends <= 1);
+        return numberOfFriends;
+    }
+
 }
