1b9ba1c78e048f7a13d8f7a78b8be1a35cb053c4
Sergey Chmutov <serik_ch@yahoo.com>
2022-11-02 19:54:04 +0300

    Первый вариант "Калькулятора счёта".

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        int countOfPersons = inputPersonCount();
+
+        CostCalculator costCalculator = new CostCalculator(countOfPersons);
+
+        costCalculator.addProducts();
+    }
+
+    private static int inputPersonCount() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int countOfGuests;
+
+        while (true) {
+            System.out.println("Введите количество гостей:");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("Вы ввели не целое число. Повторите ввод количества гостей:");
+                scanner.next();
+            }
+
+            countOfGuests = scanner.nextInt();
+            if (countOfGuests <= 1) {
+                System.out.println("Количество гостей должно быть больше 1.");
+            } else {
+                break;
+            }
+        }
+
+        return countOfGuests;
     }
 }
