97e03beca12d2a7d517d97abb39dba801afaf607
karimius <karim.it@yandex.ru>
2022-11-03 00:29:55 +0300

    Исправления в домашней работе. Вынес склонение падежей в метод. Поправил проверку на окончание для "рублей". Добавил проверки на ввод символов двумя способами.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,17 +4,42 @@ public class Calculator {
     static double sumPriceProduct = 0.00;
     static int payer;
 
-    //количество человек на чек
+    //количество человек на чек (первый способ проверки ввода символов)
     void addPeople(){
         System.out.println("На скольких человек необходимо разделить счёт?");
-        payer = Main.scanner.nextInt();
-
-        //проверка условий валидности плательщиков по чеку
-        while (payer <= 1) {
-            System.out.println("Количество человек меньше или равно одному.\nВведите число заново:");
-            payer = Main.scanner.nextInt();
+        while (true) {
+            try {
+                payer = Main.scanner.nextInt();
+                if (payer <= 1) {
+                    System.out.println("Количество человек меньше или равно одному.\nВведите число заново:");
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Вы ввели символ, а не число.\nПопробуйте ещё раз ввести количество человек");
+                Main.scanner.next();
+            }
         }
     }
+
+    //количество человек на чек (второй способ проверки ввода символов)
+    void addPeople2(){
+        System.out.println("На скольких человек необходимо разделить счёт?");
+
+        while (true) {
+            //вначале проверим число ли ввели вовсе
+            if (Main.scanner.hasNextInt()) {
+                payer = Main.scanner.nextInt();
+                if (payer <= 1) {
+                    System.out.println("Количество человек меньше или равно одному.\nВведите число заново:");
+                    break;
+                }
+            } else {
+                System.out.println("Вы ввели символ, а не число.\nПопробуйте ещё раз ввести количество человек");
+                Main.scanner.next();
+            }
+        }
+    }
+
 
     //ввод товара
     void addProduct() {
@@ -22,12 +47,17 @@ public class Calculator {
             System.out.println("Наименование товара");
             String product = Main.scanner.next();
 
-                //проверка на завершение цикла добавление товара
-                if (product.equalsIgnoreCase("Завершить")) {
-                    break;
-                }
+            //проверка на завершение цикла добавление товара
+            if (product.equalsIgnoreCase("Завершить")) {
+                break;
+            }
+
+            //вводим стоимость товара
             System.out.println("Введите стоимость товара в руб.");
-            priceProduct = Main.scanner.nextDouble();
+
+            //вначале проверим число ли ввели вовсе
+            if (Main.scanner.hasNextDouble()) {
+                priceProduct = Main.scanner.nextDouble();
 
                 //проверяем отрицательную стоимость
                 while (priceProduct < 0) {
@@ -35,41 +65,47 @@ public class Calculator {
                     priceProduct = Main.scanner.nextDouble();
                 }
 
-            //сохраняем список товаров с ценой в виде текста чтобы потом вывести список
-            listProduct = listProduct + (product + (String.format(" - %.2f", priceProduct)) +" руб.\n");
+                //сохраняем список товаров с ценой в виде текста чтобы потом вывести список
+                listProduct = listProduct + (product + (String.format(" - %.2f", priceProduct)) + " руб.\n");
 
-            //суммируем стоимость товара
-            sumPriceProduct = sumPriceProduct + priceProduct;
+                //суммируем стоимость товара
+                sumPriceProduct = sumPriceProduct + priceProduct;
 
-            System.out.println("Товар добавлен успешно.\nВведите новый товар или введите \"Завершить\", чтобы прекратить ввод товара.\n");
+                System.out.println("Товар добавлен успешно.\nВведите новый товар или введите \"Завершить\", чтобы прекратить ввод товара.\n");
+            } else {
+                System.out.println("Некорректный ввод числа.\nПопробуйте ещё раз.\n");
+                Main.scanner.next();
             }
         }
+    }
 
     //вывод чека
     void viewListProduct() {
         System.out.println("Добавленные товары:");
         System.out.println(listProduct);
+        System.out.println(String.format("Общая сумма чека - %.2f", sumPriceProduct) + GetRubleAddition() + "\n");
+        //выводим результаты рачсчёта счёта на каждого
+        System.out.println("Каждый должен заплатить - " + Math.floor(Calculator.sumPriceProduct / Calculator.payer) + GetRubleAddition());
+    }
 
-        //выводим результаты рачсчёта счёта на каждого + склонение падежа для руб.
-        double sum = Math.floor(Calculator.sumPriceProduct / Calculator.payer);
-        //вычисляем количество символом у суммы и проверяем падеж
-        String l = "" + sum;
-        String rub;
-            if (sum >= 10){
-                rub = " рублей.";
-            } else {
-                int num = (int) (sum % l.length());
-                if (num == 1) {
-                    rub = " рубль.";
-                } else if (num == 2 || num == 3 || num == 4) {
-                    rub = " рубля.";
-                }
-                else {
-                    rub = " рублей.";
-                }
-            }
-        System.out.println(String.format("Общая сумма чека - %.2f", sumPriceProduct) + rub + "\n");
-        System.out.println("Каждый должен заплатить - " + sum + rub);
+    //метод определения склонения падежа
+    public static String GetRubleAddition() {
+        double amountForEach = Math.floor(Calculator.sumPriceProduct / Calculator.payer);
+        //с 10 по 19 всегда "рублей"
+        if (amountForEach % 100 >= 10 || amountForEach % 100 < 20) {
+                return " рублей";
+        }
+        //остальные склонения по последней цифре
+        switch ((int) (amountForEach % 10)) {
+            case 1:
+                return " рубль";
+            case 2:
+            case 3:
+            case 4:
+                return " рубля";
+            default:
+                return " рублей";
+        }
     }
 }
 
