7a423524b5fa76c3650a9c135f5227b532bbcbeb
Gammus87 <shockmeshok@yandex.ru>
2022-11-07 07:03:02 +0300

    ПР #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,80 +3,88 @@ import java.util.Scanner;
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Делитель счёта v.100500. Привет!");
+        System.out.println("Делитель счёта v.7. Привет!");
         Calculate cal = new Calculate();
         cal.inputPersons();
         cal.inputItems();
         cal.payPeople();
-        cal.endRubles();
-
-
     }
-        public static class Calculate{
+        static class Calculate{
+            double price=0;
             double total=0;
-            int persons=1;
+            int persons=0;
             String items= "";
+            int payPerPerson;
+
+            String endRuble;
 
 
-
-            public void inputPersons(){
+            public  void inputPersons(){
                 while(true){
                     System.out.println("Введите количество человек:");
-                    persons= sc.nextInt();
-                    if (persons<=1){
-                        System.out.println("Людей не может быть меньше двух! Попробуйте ещё раз!");
-                        continue;
+                    if (sc.hasNextInt()) {
+                        persons = sc.nextInt();
+                    }if (persons>=2){
+                    break;
                     }else{
-                        break;}
+                        System.out.println("Некорректный ввод. Попробуйте ещё раз.");
+                        sc.nextLine();
+                             }
+                        }
                     }
-                }
+
 
             public void inputItems(){
                 while(true){
                     System.out.println("Введите название товара:");
                     String item=sc.next()+sc.nextLine();
                     System.out.println("Введите цену товара:");
-                    if (sc.hasNextDouble()){
-                        double price=sc.nextDouble();
+
+
+                    if (sc.hasNextDouble()) {
+                        price = sc.nextDouble();
+                    }if (price>2){
                         total+=price;
                         items+=item+"\n";
                     }else{
                         System.out.println("Неверный формат ввода!");
                         sc.nextLine();
-                        continue;
-                    }
+                                            }
                 System.out.println("Товар " + item + " добавлен. Добавим ещё? Да/Нет?");
                      String getMore=sc.next();
                      if (getMore.equalsIgnoreCase("нет")) {
                          break;
                      }else{
-                         continue;}
-                }
+                         }
+                    }
               }
 
               public void payPeople(){
 
               System.out.println("Добавленные товары: \n"+items);
-              double payPerPerson=total/persons;
-                  System.out.println("Каждый из " + persons + " человек заплатит по " + String.format("%.2f",total/persons));
+              payPerPerson=(int)total/persons;
+              double pppWithoutRemains=(double) payPerPerson; // по другому я просто не знаю как игнорировать копейки при объявлении окончания рубля, поэтому пришлось убрать их вовсе,
+                  endRubles();//другого способа как их заигнорить я не нашёл, так что не судите строго.
+                  System.out.println("Каждый из " + persons + " человек заплатит по " + String.format("%.2f",pppWithoutRemains)+ " " +endRuble+ ".");
+
             }
 
             public void endRubles() {
 
-                String endRuble;
-                if (total/persons == 1) {
-                    endRuble = "Рубль";
-                } else if (total/persons <= 4) {
-                    endRuble = "Рубля";
-                } else {
-                    endRuble = "Рублей";
 
-                }
-                System.out.println(endRuble); // это шок
-                //Я не понимаю как иначе вывести переменную endRuble. Её нельзя добавить в верхний метод.
-            }   //Если поменять параметры метода, то потом он просто не встанет в Main.
-             }
+                if (payPerPerson % 10 == 1) {
+                    endRuble = "рубль";
+                } else if (payPerPerson % 10 == 2 || payPerPerson % 10 == 3 || payPerPerson % 10 == 4) { // копейки всё портят.
+                    endRuble = "рубля";
+                } else {
+                    endRuble = "рублей";
+
+                              }
+
+                     }
 
 
             }
+
+}
 
