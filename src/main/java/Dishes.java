08a8e556068f3f34fb8453c5c2c1a3d9e4d31e65
UltravioletDays <ale99651550@yandex.ru>
2022-11-09 02:18:03 +0300

    Черновик

---
+++ src/main/java/Dishes.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Dishes {
+    String name = "";
+    float price;
+    //Поля
+    Scanner scanner = new Scanner(System.in);
+    //Бесконечный цикл вопросов о блюдах и цен на них
+    name += name;
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        byte friends;
+
+
+
+        while (true) {
+            System.out.println("Введите числовое количество участников:");
+            Scanner input = new Scanner(System.in);
+            if (input.hasNextByte()) {
+                friends = input.nextByte();
+                if (friends <= 1) { System.out.println("Нет смысла делить счёт. Попробуйте ещё раз.");}
+                else if (friends > 1) {
+                    break;
+                }
+            }
+            else {
+                System.out.println("Требуется указать числовое значение!");
+            }
+        }
+
+        //Вызов Dishes
+
+
+        //Окончания рублей (oh no...)
+        String result = "";
+        float total;
+
+        int num100 = (int) (Math.floor(total % 100));
+        if (num100 > 4 && num100 < 21) {
+            result = "Рублей";
+        } else {
+            int num10 = num100 % 10;
+            if (num10 == 1) {
+                result = "Рубль";
+            } else if (num10 > 1 && num10 < 5) {
+                result = "Рубля";
+            } else{
+                result = "Рублей";
+            }
+        }
+
+
+        System.out.print("Добавленные товары: " + storeProducts);
+        System.out.println("Сумма к оплате каждым гостем:" + String.format("%.2f", finalResult) + " " + ending);
     }
 }
