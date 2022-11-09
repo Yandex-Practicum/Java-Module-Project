f9d820ef4c80d4b0de9cae49e7f966135c5a6b08
UltravioletDays <ale99651550@yandex.ru>
2022-11-09 20:16:49 +0300

    Черновик

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
                 friends = input.nextByte();
                 if (friends <= 1) {
                     System.out.println("Нет смысла делить счёт. Попробуйте ещё раз.");
-                } else if (friends > 1) {
+                } else {
                     break;
                 }
             } else {
