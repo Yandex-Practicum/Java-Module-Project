3043ddbe26d983621a2e0e4b4b8f880a0a507e53
wire-less <eastbalt.ru@gmail.com>
2022-11-09 21:56:58 +0300

    Sprint2 task release 1.1 "galop"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,27 +9,37 @@ public class Main {
 
         // Добавляем блюда к счету
         Receipt tonightParty = new Receipt();
-        Scanner dishScan1 = new Scanner(System.in);
-        Scanner dishScan2 = new Scanner(System.in);
+        Scanner menuTitle = new Scanner(System.in);
+        Scanner menuTitlePrice = new Scanner(System.in);
         String dishTitle;
         float dishCost;
 
         while (true) {
             System.out.println("\nХотите добавить еще одно блюдо в счет? Нет - наберите \"завершить\"; Да - введите наименование блюда: ");
-            dishTitle = dishScan1.nextLine();
+            dishTitle = menuTitle.nextLine();
 
             if (dishTitle.equalsIgnoreCase("ЗАВЕРШИТЬ"))
                 break;
 
             System.out.println("Введите стоимость блюда (руб,коп): ");
-            dishCost = dishScan2.nextFloat();
-
+            while (true) {
+                if (!menuTitlePrice.hasNextFloat()) {  // Проверем, что это дробное число
+                    System.out.println("Требуется числовое значение цены блюда: ");
+                    menuTitlePrice.next(); // Пропускаем шум вместо нужного ввода
+                } else {
+                    dishCost = menuTitlePrice.nextFloat();
+                    if (dishCost < 0)
+                        System.out.println("Требуется положительное значение цены блюда: ");
+                  else
+                      break;
+                    }
+                }
             tonightParty.addDish(dishTitle);
             tonightParty.addSum(dishCost);
-        }
+            }
 
-        printOut( count );
 
+        printOut( count );  // Печатаем результаты
     }
 
     public static byte askPersonCount() { // Метод входных параметров для счетчика
@@ -58,31 +68,50 @@ public class Main {
     }
 
     public static void printOut(int persons) {    // Выводим результаты
-        String ending;
+        String ending = "";
 
-        switch ( (int)((Math.floor(Receipt.totalPrice / persons)) % 10)) {
-            case 1:
-                ending = "рубль";
-                break;
-            case 2:
-            case 3:
-            case 4:
-                ending = "рубля";
-                break;
-            case 0:
+        int average = (int) Math.floor(Receipt.totalPrice / persons);
+        int averageTens = average % 100;
+        int averageUnits = average % 10;
+
+        if ((10 < averageTens) && (averageTens < 20)) {
+            switch ( averageTens ) {
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                    ending = "рублей";
+                    break;
+            }   // no default
+        } else {
+
+            switch (averageUnits) {
+                case 1:
+                    ending = "рубль";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    ending = "рубля";
+                    break;
+                case 0:
                 case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-                ending = "рублей";
-                break;
-            default:
-                ending = "рублей";
-                break;
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    ending = "рублей";
+                    break;
+                default:
+                    ending = "рублей";
+                    break;
+            }
         }
-
         System.out.println("\n\nВаш заказ сегодня:\n" + Receipt.totalDishes);
         System.out.println("\nОбщая сумма к оплате: " + String.format("%.2f", Receipt.totalPrice));
         System.out.println("\n\nКоличесво гостей: " + persons);
