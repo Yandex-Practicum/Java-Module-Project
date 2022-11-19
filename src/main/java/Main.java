797cebf7b7c3a5ec8c57049a4672989b275b605c
Maxim Berezin <beresin.m@googlemail.com>
2022-11-19 19:07:08 +0300

    add people counter logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Hello world!");
+        System.out.println("На скольких человек необходимо разделить счёт?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int peopleCounter;
+
+        while (true){
+
+            if (scanner.hasNextInt()){
+                peopleCounter = scanner.nextInt();
+                if (peopleCounter == 1){
+                    System.out.println("В этом случае нет смысла ничего считать и делить. Пожалуйста введите корректное значение для подсчёта.");
+                } else if(peopleCounter < 1){
+                    System.out.println("Количество человек меньше 1. Пожалуйста введите корректное значение для подсчёта.");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("Пожалуйста введите корректное значение для подсчёта.");
+                scanner.next();
+            }
+
+        }
+
+
     }
 }
