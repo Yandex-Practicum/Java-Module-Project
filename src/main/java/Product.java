359b760c60584b3adc3e729923bd5cf315ebec6e
Maxim Berezin <beresin.m@googlemail.com>
2022-11-19 19:21:21 +0300

    add constructor for Product

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,11 @@
 public class Product {
+
+    private double price;
+    private String name;
+
+    public Product(double price, String name){
+        this.price = price;
+        this.name = name;
+    }
+
 }
