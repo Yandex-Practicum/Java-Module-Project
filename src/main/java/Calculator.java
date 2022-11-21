db520fa8221a8ac06a5e0bb6c0b576de5bc6b60a
Maxim Berezin <beresin.m@googlemail.com>
2022-11-21 22:33:32 +0300

    add new methods to show calculation result

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -13,4 +13,31 @@ public class Calculator {
         System.out.println("Товар успешно добавлен.");
     }
 
+    public void showAddedProducts(){
+        System.out.println("\n" + addedProducts);
+    }
+
+    public void divide(){
+        double dividedPrice = totalPrice / (double) peopleCounter;
+        String formattedPrice = formatPrice(dividedPrice);
+        String currency = getCurrencyWriting(dividedPrice);
+
+        System.out.println("Каждый человек должен заплатить " + formattedPrice + " " + currency + ".");
+    }
+
+    private String formatPrice(double price){
+        return String.format("%,.2f", price);
+    }
+
+    private String getCurrencyWriting(double price){
+        int roundedPrice = (int)Math.floor(price);
+        if (roundedPrice == 1) {
+            return "рубль";
+        } else if (roundedPrice >= 2 &&
+                   roundedPrice <= 4) {
+            return "рубля";
+        } else {
+            return "рублей";
+        }
+    }
 }
