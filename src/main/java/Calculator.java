c7d41f90642e73a440a323059c8633196c9aa195
Maxim Berezin <beresin.m@googlemail.com>
2022-11-21 21:28:22 +0300

    add addProduct-method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,16 @@
 public class Calculator {
     private int peopleCounter;
+    private String addedProducts = "Добавленные товары:\n";
+    private double totalPrice;
 
     public Calculator(int peopleCounter){
         this.peopleCounter = peopleCounter;
     }
+
+    public void addProduct(Product product){
+        addedProducts = addedProducts + product.getName() + "\n";
+        totalPrice += product.getPrice();
+        System.out.println("Товар успешно добавлен.");
+    }
+
 }
