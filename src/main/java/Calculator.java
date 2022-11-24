f7fae870b408fc5776bde3f83663d4fec9a8f650
admin <ekimovadaria7@gmail.com>
2022-11-25 00:39:05 +0300

    Калькулятор

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Calculator {
+    public void productsLists( Scanner scanner) {
+        String answer = "Добавленные товары:";
+        String productName;
+        float price = 0;
+        float sum=0.00f;
+        System.out.println("Введите наименование товара");
+        productName = scanner.next();
+        System.out.println("Введите стоимость товара в формате 'рубли.копейки'");
+        while(price<=0){
+            if (scanner.hasNextFloat()) price = scanner.nextFloat();
+            else System.out.println("Введите корректное значение больше 0.0");
+        }
+        answer+="\n" + productName;
+        sum += price;
+        System.out.println("товар успешно добавлен!\nХотите добавить ещё?");
+        switch(scanner.next()) {
+            case "Да":
+                productsLists(scanner);
+            case "Завершить":
+                System.out.println(answer);
+                System.out.println("Общая сумма"+sum);
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        Scanner scanner = new Scanner(System.in);
+        int people = addPerson(scanner);
+        Calculator calculator = new Calculator();
+        for(int i = 0; i <= people; i++) {
+            calculator.productsLists(scanner);
+        }
+    }
+
+    public static int addPerson(Scanner scanner) {
+        int people = 0;
+        boolean hasPeople = false;
+        while(!hasPeople) {
+            System.out.println("Введите количество человек:");
+            if (scanner.hasNextInt()) {
+                people = scanner.nextInt();
+                if (people > 1) {
+                    hasPeople = true;
+                } else {
+                    System.out.println("Введено неверное количество! попробуйте еще раз.");
+                }
+            }else {
+                System.out.println("Введено неверное количество! попробуйте ввести количество гостей в виде числа.");
+                scanner.nextLine();
+            }
+        }
+        return people;
     }
 }
