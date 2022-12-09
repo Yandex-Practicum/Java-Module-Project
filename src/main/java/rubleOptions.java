574a740943b4af045d604032d52735f1ccab22a1
1sleepwalker1 <serzh-belov-94@list.tu>
2022-12-10 01:16:14 +0300

    Работа 2

--- src/main/java/rubleOptions.java
+++ src/main/java/rubleOptions.java
@@ -1,15 +1,19 @@
 public class rubleOptions {
 
 
-    String rubles(double prise) {
-       double rubles = prise;
+    String rubles(double value) {
+        double rubles = Math.floor(value);
         if (rubles == 1) {
             return " рубль";
-        } else if (rubles >= 2 && rubles <= 4) {
+        } else if (rubles == 12 || rubles == 13 || rubles == 14  ) {
+            return " рублей";
+        } else if (rubles%10 >= 2 && rubles%10 <= 4) {
             return " рубля";
-        } else
+        } else if (rubles%100 >= 2 && rubles%100 <= 4) {
+            return " рубля";
+        } else {
             return " рублей";
         }
-
     }
+}
 
